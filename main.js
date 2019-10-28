e911bb1f33eac6b2dd6c0503cf75d05e37f4b705
ndhammer <ndlyga@apphammer.co>
2019-10-28 15:23:55 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-class TodoApp extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-  
-    render() {
-      return (
-        <div>My Todo App!</div>
-      );
-    }
-  }
-  
-
-ReactDOM.render(
-    <TodoApp />,
-    document.getElementById('todoApp')
-  );
+// You code goes here!
